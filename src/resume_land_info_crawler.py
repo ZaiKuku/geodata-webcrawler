a368a75a33e3aab0aeaab1ac963f3db431ce20b6
DESKTOP-MJDT8G6\user <willzau24531@mail.datayoo.com.tw>
2024-07-18 13:58:05 +0800

    fix: fix the bug of resume_land_info_crawler.py, add .gitignore

--- src/resume_land_info_crawler.py
+++ src/resume_land_info_crawler.py
@@ -169,9 +169,11 @@ def get_s_n_id_geometry(unit_id, section_id, full_land_no):
         },
         headers=request_headers
     )
-
-    response_content = response.json()
+    try:
+        response_content = response.json()
     # print(response_content)
+    except Exception as e:
+        print(f"An error occurred while fetching land info of SID: {unit_id}{section_id} NID: {full_land_no} - {e}")
 
     if response_content['success']:
         print(f"Successfully fetched land info of trace code: {full_land_no}")
